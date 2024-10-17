494dbdd235e1e47ef17ec0ba3a2143fdbe4fb436
Keks <keks@htmlacademy.ru>
2024-10-17 15:03:54 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/functions.js
+++ 2/js/functions.js
@@ -3,7 +3,7 @@ function checkLength(string, maxLength) {
 }
 
 function reverse(s) {
-  return s.split("").reverse().join("");
+  return s.split('').reverse().join('');
 }
 
 function isPalindrome(string) {
@@ -11,10 +11,37 @@ function isPalindrome(string) {
   return reverse(lowerString) === lowerString;
 }
 
-function extractDigits(str) {
-  const digits = String(str).match(/\d+/g);
+function extractDigits(string) {
+  const digits = String(string).match(/\d+/g);
   if (!digits) {
     return NaN;
   }
   return parseInt(digits.join(''), 10);
 }
+
+// Cтрока короче 20 символов
+checkLength('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+checkLength('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+checkLength('проверяемая строка', 10); // false
+
+// Строка является палиндромом
+isPalindrome('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+isPalindrome('ДовОд'); // true
+// Это не палиндром
+isPalindrome('Кекс'); // false
+
+// Это палиндром
+isPalindrome('Лёша на полке клопа нашёл '); // true
+
+extractDigits('2023 год'); // 2023
+extractDigits('ECMAScript 2022'); // 2022
+extractDigits('1 кефир, 0.5 батона'); // 105
+extractDigits('агент 007'); // 7
+extractDigits('а я томат'); // NaN
+
+extractDigits(2023); // 2023
+extractDigits(-1); // 1
+extractDigits(1.5); // 15
